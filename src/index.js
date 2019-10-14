fea7c5da091ecd2fb4d7b79b942a41ed7888a05b
prostiak2019 <yarovenkodmv@gmail.com>
2019-10-14 15:16:49 +0300

    feat: add task

--- src/index.js
+++ src/index.js
@@ -37,67 +37,78 @@ const MORSE_TABLE = {
     '-----':  '0',
 };
 
+/**
+ * Decodes encoded data.
+ *
+ * @param {string} expr - encoded string
+ * @return {string} signsDecode - decoded string
+ */
 
 function decode(expr) {
 
-//   *** part I ***
-  let morsePoint = Object.keys(MORSE_TABLE);
-  var i = 0;
+
+  // get all the codes of morse object
+  const morsePoint = Object.keys(MORSE_TABLE);
+
+  // get all the values of morse object
+  const morseSignFull = Object.values(MORSE_TABLE);
+
+  let i = 0;
   let morseBulean = [];
-  
+
+   // loop all the morse codes to convert '.' to '10' and '-' to '11'
   for (var key in MORSE_TABLE) {
 
-    morseBulean [i] = '';
+    morseBulean[i] = '';
 
     for (let j = 0; j < morsePoint[i].length ; j++) { 
 
-        if (morsePoint[i].slice(j, j + 1) === '.') {
-            morseBulean[i] = morseBulean [i] + "10";
-        } else {
-            morseBulean[i] = morseBulean [i] + "11";
-        }
-    } 
+      if (morsePoint[i].slice(j, j + 1) === '.') {
+        morseBulean[i] = morseBulean[i] + '10';
+        } else { morseBulean[i] = morseBulean[i] + '11';
+      }
+    }
+
     i++;
   }
   
-  let zero ="0";
+  let zero ='0';
 
+  // loop to add '0' to the beginning of an element if the number of signs < 10
   let morseBuleanFull = morseBulean.map(function(sign) {
     while (sign.length !=10) {
-        sign = zero +sign
-      } 
+      sign = zero + sign;
+    }
+
     return sign;
   });
     
-  morseBuleanFull.push("**********");
+  // add the element '**********' to the end of the array, which matches the value ' '
+  morseBuleanFull.push('**********');
 
-  let morseSignFull = Object.values(MORSE_TABLE);
+  // add the element ' ' to the end of the array, which matches the value '**********'
+  morseSignFull.push(' ');
 
-  morseSignFull.push(" ");
-
-//    ***part II***
+  // break the input variable into 10 signs 
 
   let slicedExpr = expr.match(/.{1,10}/g);
  
-//    ***part III***
-
   let signsDecode = '';
 
+  // decoding cycle from zeros and ones to alphabet and numbers
   for (let i = 0; i < slicedExpr.length; i++) {
     for (let j = 0; j < morseBuleanFull.length; j++) {
-        let valueSlicedExpr = slicedExpr[i];
-        let valueMorseBuleanFull = morseBuleanFull[j];
-        if (valueSlicedExpr == valueMorseBuleanFull) {
-          signsDecode = signsDecode + morseSignFull[j];
-        }        
+      let valueSlicedExpr = slicedExpr[i];
+      let valueMorseBuleanFull = morseBuleanFull[j];
+      if (valueSlicedExpr == valueMorseBuleanFull) {
+        signsDecode = signsDecode + morseSignFull[j];
+      }        
     }      
   }
 
-console.log ("signsDecode", signsDecode);
-
- return signsDecode;
+  return signsDecode;
 }
 
- module.exports = {
+module.exports = {
     decode
 }
